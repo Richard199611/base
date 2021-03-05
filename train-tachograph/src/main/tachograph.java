54cf832314a9bacaf7757448d65bc784cef48114
Richard <Dell@DESKTOP-2ENJN2B>
2021-03-05 10:53:54 +0100

    change the code

--- train-tachograph/src/main/tachograph.java
+++ train-tachograph/src/main/tachograph.java
@@ -1,10 +1,13 @@
 import java.util.Date;
+implementation 'org.checkerframework.annotatedlib:guava:30.1-jre'
+
+
 
 public class tachograph {
     private TrainSystem trainSys;
 
     public Table<String, Integer, Integer> TrainDataTable;
-    
+
     void addToTrainTable() {
         Date date = new Date()
         TrainDataTable.put(date.toString(), trainSys.getReferenceSpeed(), trainSys.TrainUser.getJoystickPosition()); 
